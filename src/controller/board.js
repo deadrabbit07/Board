23b35dca2b5d51a0444d6a25cc98288aef17ad63
Gjs977 <sdh230307@sdh.hs.kr>
2025-05-14 09:01:55 +0900

    게시글 작성 조회수 빼고 성공

--- src/controller/board.js
+++ src/controller/board.js
@@ -5,25 +5,28 @@ exports.post = (req, res) => {
     const post_title = req.body.post_title; 
     const post_content = req.body.post_content;
 
-    console.log(`user_id: ${user_id}, post_title: ${post_title}`); //로그인한 id랑 쓴 글의 title 서버에서 출력
-
-    const currentDate = new Date().toISOString().slice(0, 10); // 현재 날짜를 'YYYY-MM-DD' 형식으로 가져오기
-
-    db.query("INSERT INTO board (title, content, date, maker) VALUES (?, ?, ?, ?);", [post_title, post_content, currentDate, user_id], (err, result) => {
-        if (err) {
-            return res.status(500).json("서버 오류");
-        } else {
-            return res.status(200).json({ message: "글작성 성공" });
+    db.query(
+        "INSERT INTO board (title, content, date, maker) VALUES (?, ?, CURDATE(), ?);",
+        [post_title, post_content, user_id],
+        (err, result) => {
+            if (err) {
+                return res.status(500).json("서버 오류");
+            } else {
+                return res.status(200).json({ message: "글작성 성공" });
+            }
         }
-    });
+    );
 };
 
-exports.board = (req, res) =>{  
-    db.query("SELECT * from board;", (err, result) => {
-        if (err){
-            return res.status(500).json();
-        }else{
-            return res.status(200).json(result);
+exports.board = (req, res) => {
+    db.query(
+        "SELECT title, content, DATE_FORMAT(date, '%Y-%m-%d') AS date, maker FROM board;",
+        (err, result) => {
+            if (err) {
+                return res.status(500).json("서버 오류");
+            } else {
+                return res.status(200).json(result);
+            }
         }
-    })
-}
+    );
+};
